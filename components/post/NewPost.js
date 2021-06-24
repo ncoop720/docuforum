557a5d47672204857dd78723f868b30d1eec1c9b
Nathan Cooper <ncoop720@yahoo.com>
2021-06-23 21:25:50 -0400

    Basic editor

--- components/post/NewPost.js
+++ components/post/NewPost.js
@@ -1,9 +1,25 @@
 import Modal from '@material-ui/core/Modal'
+import Container from '@material-ui/core/Container'
+import dynamic from "next/dynamic"
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+
+const Editor = dynamic(
+  () => import('react-draft-wysiwyg').then(mod => mod.Editor),
+  { ssr: false }
+)
+
+const containerStyle = {
+  backgroundColor: 'white',
+  marginTop: '20px',
+  padding: '10px'
+}
 
 const NewPost = ({ closeNewPost, newPostOpen }) => {
   return (
     <Modal open={newPostOpen} onClose={() => closeNewPost()}>
-      <p>placeholder</p>
+      <Container style={containerStyle} maxWidth="sm">
+        <Editor />
+      </Container>
     </Modal>
   )
 }
