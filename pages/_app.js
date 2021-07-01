ccc7df1965e5a7842b386797f3ca4b44a24307e3
Nathan Cooper <ncoop720@yahoo.com>
2021-06-30 21:59:06 -0400

    Offset container left margin to show content

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import '../styles/globals.css'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/client'
 import { useState } from 'react'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
-import Container from '@material-ui/core/Container'
-import SideMenu from '../components/SideMenu'
 import BottomNav from '../components/BottomNav'
+import SideMenu from '../components/SideMenu'
+import '../styles/globals.css'
 
 const navIndex = {
   'feed': 0,
@@ -14,18 +13,29 @@ const navIndex = {
   'users': 2
 }
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   const router = useRouter()
   const [ session, loading ] = useSession()
   const [navHighlight, setNavHighlight] = useState(navIndex[router.pathname.split('/')[1]])
-  const largeDevice = useMediaQuery('(min-width:1280px)')
+  const largeDevice = useMediaQuery('(min-width:960px)')
+
+  function containerOffset() {
+    if (largeDevice)
+      return '200px'
+    else
+      return '0px'
+  }
+
+  const containerStyle = {
+    marginLeft: containerOffset()
+  }
 
   return (
     <>
       {largeDevice && <SideMenu navHighlight={navHighlight} setNavHighlight={setNavHighlight} />}
-      <Container maxWidth="sm">
+      <div style={containerStyle}>
         <Component {...pageProps} session={session} loading={loading} />
-      </Container>
+      </div>
       {!largeDevice && <BottomNav navHighlight={navHighlight} setNavHighlight={setNavHighlight} />}
     </>
   )
