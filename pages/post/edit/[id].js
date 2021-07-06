18421629aa75ac0492ffd57f348f8c2f70e2b0f5
Nathan Cooper <ncoop720@yahoo.com>
2021-07-06 19:40:14 -0400

    Post edit page - back button

--- pages/post/edit/[id].js
+++ pages/post/edit/[id].js
@@ -1,3 +1,7 @@
+import AppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton'
+import Toolbar from '@material-ui/core/Toolbar'
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
@@ -9,5 +13,16 @@ export default function editPost({ session, loading }) {
     if (!loading && !session) router.push('/api/auth/signin')
   }, [loading])
 
-  return <div>edit post {id}</div>
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton edge="start" onClick={() => router.back()}>
+            <KeyboardBackspaceIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <div>edit post {id}</div>
+    </>
+  )
 }
