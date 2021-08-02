d235425bca222437a3a1f1e020d0c4cfb6f7c2cd
unknown <ncoop720@yahoo.com>
2021-08-02 19:45:58 -0400

    Monaco Editor for post edit

--- pages/post/edit/[id].js
+++ pages/post/edit/[id].js
@@ -2,34 +2,21 @@ import AppBar from '@material-ui/core/AppBar'
 import IconButton from '@material-ui/core/IconButton'
 import Toolbar from '@material-ui/core/Toolbar'
 import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace'
-import { convertToRaw, EditorState } from 'draft-js'
 import { useRouter } from 'next/router'
 import { getSession } from 'next-auth/client'
 import { useState } from 'react'
-import PostSection from '../../../components/PostSection'
+import Editor from '@monaco-editor/react'
 
 export async function getServerSideProps({ req, query }) {
   const session = await getSession({ req })
   if (!session) return { redirect: { destination: '/api/auth/signin' } }
   const { id } = query
-
-  if (id === 'new')
-    return {
-      props: {
-        initialPostSections: [
-          {
-            content: convertToRaw(EditorState.createEmpty().getCurrentContent()),
-            id: 0,
-            type: 'text'
-          }
-        ]
-      }
-    }
+  if (id === 'new') return { props: { initialCode: '' } }
 }
 
-export default function editPost({ initialPostSections }) {
+export default function editPost({ initialCode }) {
   const router = useRouter()
-  const [postSections, setPostSections] = useState(initialPostSections)
+  const [code, setCode] = useState(initialCode)
   const containerStyle = { padding: '10px' }
 
   return (
@@ -42,16 +29,12 @@ export default function editPost({ initialPostSections }) {
         </Toolbar>
       </AppBar>
       <div style={containerStyle}>
-        {postSections.map(({ content, id, type }) => {
-          return (
-            <PostSection
-              content={content}
-              key={id}
-              type={type}
-              setPostSections={setPostSections}
-            />
-          )
-        })}
+        <Editor
+          defaultLanguage="html"
+          defaultValue={code}
+          height='calc(95vh - 64px)'
+          onChange={newCode => setCode(newCode)}
+        />
       </div>
     </>
   )
