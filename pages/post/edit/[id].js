b691a89bf9320498ae795cd65866f0df46e3ac4b
unknown <ncoop720@yahoo.com>
2021-09-05 10:37:14 -0400

    Focus on new text section when it gets added

--- pages/post/edit/[id].js
+++ pages/post/edit/[id].js
@@ -15,18 +15,25 @@ import PostSection from '../../../components/PostSection'
 import PostSectionAdd from '../../../components/PostSectionAdd'
 import PostSectionText from '../../../components/PostSectionText'
 
-export default function PostEdit({ AppAPI, id, initialPost }) {
+export default function PostEdit({ AppAPI, initialPost }) {
   const { midDevice, largeDevice, router } = AppAPI
   const [navHighlight, setNavHighlight] = useState(0)
   const [post, setPost] = useState(initialPost)
-  const [newId, setNewId] = useState(Math.max(post.sections.map(s => s.id)) + 1)
+  const { sections, title } = post
+  const maxPostSectionId = Math.max(...sections.map(s => s.id))
+  const [newId, setNewId] = useState(maxPostSectionId + 1)
   const PostEditAPI = { newId, post, setNewId, setPost }
   const editorShow = (largeDevice || navHighlight === 0) ? {} : { display: 'none' }
   const previewShow = (largeDevice || navHighlight === 1) ? {} : { display: 'none' }
   const containerStyle = { display: 'flex', height: containerHeight(), justifyContent: 'space-around' }
   const subContainerStyle = { padding: '5px', width: largeDevice ? '50%' : '100%' }
   const editorSubContainerStyle = { ...subContainerStyle, ...editorShow, overflowY: 'scroll' }
-  useEffect(() => { if (id === 'new') document.getElementsByClassName('public-DraftEditor-content')[0].focus() }, [])
+
+  useEffect(() => {
+    const sectionToFocus = sections.filter(section => section.id === maxPostSectionId)[0]
+    if (sectionToFocus.type === 'text')
+      document.getElementsByClassName('public-DraftEditor-content')[sections.indexOf(sectionToFocus)].focus()
+  }, [sections.length])
 
   return (
     <>
@@ -34,7 +41,7 @@ export default function PostEdit({ AppAPI, id, initialPost }) {
         <Toolbar>
           <IconButton edge="start" onClick={() => router.back()}><KeyboardBackspaceIcon /></IconButton>
           <TextField
-            defaultValue={post.title}
+            defaultValue={title}
             label="Title"
             onChange={e => setPost({ ...post, title: e.target.value })}
             variant="filled" />
@@ -43,7 +50,7 @@ export default function PostEdit({ AppAPI, id, initialPost }) {
       </AppBar>
       <div style={containerStyle}>
         <div id="editor-subcontainer" style={editorSubContainerStyle}>
-          {post.sections.map(({ id, type }, sectionIndex) => { return (
+          {sections.map(({ id, type }, sectionIndex) => { return (
             <Fragment key={id}>
               <PostSectionAdd PostEditAPI={PostEditAPI} sectionIndex={sectionIndex} />
               <PostSection PostEditAPI={PostEditAPI} sectionIndex={sectionIndex}>
@@ -53,7 +60,7 @@ export default function PostEdit({ AppAPI, id, initialPost }) {
               </PostSection>
             </Fragment>
           )})}
-          <PostSectionAdd PostEditAPI={PostEditAPI} sectionIndex={post.sections.length} />
+          <PostSectionAdd PostEditAPI={PostEditAPI} sectionIndex={sections.length} />
         </div>
         <div id="preview-subcontainer" style={{ ...subContainerStyle, ...previewShow }} />
       </div>
@@ -85,7 +92,7 @@ export async function getServerSideProps({ req, query }) {
   if (!session) return { redirect: { destination: '/api/auth/signin' } }
   const { id } = query
 
-  if (id === 'new') return { props: { id, initialPost: {
+  if (id === 'new') return { props: { initialPost: {
     sections: [{ content: convertToRaw(EditorState.createEmpty().getCurrentContent()), id: 0, type: 'text' }],
     title: 'Untitled'
   }}}
